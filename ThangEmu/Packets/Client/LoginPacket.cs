b39d3051adc04e2507055cc80d8870fc37c3ce09
GrzegorzBlaszczynski <grzegorz.blaszczynski@gmail.com>
2024-10-13 18:58:53 +0200

    Changed password handling and added the ability to enter the game.

---
+++ ThangEmu/Packets/Client/LoginPacket.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThangEmu.Packets.Tools;
+
+namespace ThangEmu.Packets.Client
+{
+    public class PacketEntryRead
+    {
+        public short opCode;
+
+        public PacketEntryRead(byte[] packet)
+        {
+            Read(packet);
+        }
+        public void Read(byte[] packet)
+        {
+            int i = 0;
+            opCode = BitConverter.ToInt16(PacketTools.XORArrays(PacketTools.GetBytesAtIndex<short>(packet, 0)));
+
+        }
+    }
+
+    public class LoginPacket
+    {
+        public short opCode;
+        public short unknow1;
+        public short unknow2;
+        public short LoginLen;
+        public string Login;
+        public short PasswordLen;
+        public string Pasword;
+        public int unknow3;
+
+
+
+        public LoginPacket(byte[] packet)
+        {
+            Read(packet);
+        }
+
+
+
+        public void Read(byte[] packet)
+        {
+            int i = 0;
+            opCode = BitConverter.ToInt16(PacketTools.XORArrays(PacketTools.GetBytesAtIndex<short>(packet, 0)));
+            i += 2;
+            unknow1 = BitConverter.ToInt16(PacketTools.XORArrays(PacketTools.GetBytesAtIndex<short>(packet, i)));
+            i += 2;
+            unknow2 = BitConverter.ToInt16(PacketTools.XORArrays(PacketTools.GetBytesAtIndex<short>(packet, i)));
+            i += 2;
+            LoginLen = BitConverter.ToInt16(PacketTools.XORArrays(PacketTools.GetBytesAtIndex<short>(packet, i)));
+            i += 2;
+            Login = Encoding.ASCII.GetString(PacketTools.XORArrays(PacketTools.GetBytesAtIndex(packet, i, LoginLen)));
+            i += LoginLen;
+            PasswordLen = BitConverter.ToInt16(PacketTools.XORArrays(PacketTools.GetBytesAtIndex<short>(packet, i)));
+            i += 2;
+            Pasword = Encoding.ASCII.GetString(PacketTools.XORArrays(PacketTools.GetBytesAtIndex(packet, i, PasswordLen)));
+        }
+    }
+}
---
+++ ThangEmu/Packets/Tools/PacketTools.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThangEmu.Packets.Tools
+{
+    public class PacketTools
+    {
+        static byte[] password = new byte[] { 0x77, 0x00, 0x00, 0x00 };
+
+        public static unsafe byte[] GetBytesAtIndex<T>(byte[] data, int index)
+        {
+            int count = sizeof(T);
+
+            if (index < 0 || index + count > data.Length)
+            {
+                throw new IndexOutOfRangeException("Index Out Of Array");
+            }
+
+            byte[] result = new byte[count];
+
+            Array.Copy(data, index, result, 0, count);
+
+            return result;
+        }
+        public static unsafe byte[] GetBytesAtIndex(byte[] data, int index, int count)
+        {
+
+
+            if (index < 0 || index + count > data.Length)
+            {
+                throw new IndexOutOfRangeException("Index Out Of Array");
+            }
+
+            byte[] result = new byte[count];
+
+            Array.Copy(data, index, result, 0, count);
+
+            return result;
+        }
+
+        public static byte[] XORArrays(byte[] data)
+        {
+            byte[] result = new byte[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                result[i] = (byte)(data[i] ^ password[i % password.Length]);
+            }
+
+            return result;
+        }
+    }
+}
